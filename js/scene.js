f2a2634ffbc2b556c0fa6218f1ae1cff56a67604
Jumas Cola <kbbyfl91@gmail.com>
2021-12-17 11:20:04 +0300

    add chisburger

--- js/scene.js
+++ js/scene.js
@@ -110,7 +110,7 @@ class App {
   renderScene() {
     const scene = this.scene;
 
-    this.loader.load( 'models/a_class_room/scene.gltf', function ( gltf ) {
+    this.loader.load( 'models/cheeseburger.glb', function ( gltf ) {
 
       scene.add( gltf.scene );
 
